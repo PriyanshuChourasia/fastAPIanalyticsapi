c61caab09229971c579a0cdd669bfeb41114e8d1
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-07-18 12:24:47 +0530

    post route and return schema update

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -6,11 +6,23 @@ router = APIRouter()
 @router.get("/")
 def read_events():
     return{
-        "items":[1,2,3]
+        "items":[
+            {"id":1},
+            {"id":2},
+            {"id":3}
+        ],
+        "count":3
     }
 
 @router.get("/{event_id}")
 def get_event(event_id:int) -> EventSchema:
     return{
         "id":event_id
+    }
+
+# Create event
+@router.post("/")
+def create_event() -> EventSchema:
+    return{
+        "id":123
     }
